9ee638654385c4f5a8f0b5aa88db256599275f9b
Pedro Bressan <phmbressan@usp.br>
2024-09-19 18:18:36 -0300

    ENH: provide from_dict classmethods for decoding basic classes.

--- rocketpy/mathutils/function.py
+++ rocketpy/mathutils/function.py
@@ -5,9 +5,7 @@ and more. This is a core class of our package, and should be maintained
 carefully as it may impact all the rest of the project.
 """
 
-import base64
 import warnings
-import zlib
 from bisect import bisect_left
 from collections.abc import Iterable
 from copy import deepcopy
@@ -26,6 +24,8 @@ from scipy.interpolate import (
 )
 
 from ..plots.plot_helpers import show_or_save_plot
+
+from rocketpy._encoders import from_hex_decode, to_hex_encode
 
 # Numpy 1.x compatibility,
 # TODO: remove these lines when all dependencies support numpy>=2.0.0
@@ -3432,7 +3432,7 @@ class Function:  # pylint: disable=too-many-public-methods
         source = self.source
 
         if callable(source):
-            source = zlib.compress(base64.b85encode(dill.dumps(source))).hex()
+            source = to_hex_encode(source)
 
         return {
             "source": source,
@@ -3454,9 +3454,7 @@ class Function:  # pylint: disable=too-many-public-methods
         """
         source = func_dict["source"]
         if func_dict["interpolation"] is None and func_dict["extrapolation"] is None:
-            source = dill.loads(
-                base64.b85decode(zlib.decompress(bytes.fromhex(source)))
-            )
+            source = from_hex_decode(source)
 
         return cls(
             source=source,
